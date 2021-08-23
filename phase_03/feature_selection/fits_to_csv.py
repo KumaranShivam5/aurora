746f99df0e703e9d70f93c3cb0ae2348af881b6f
KumaranShivam5 <kumaranshivam57@gmail.com>
2021-08-23 10:55:14 +0530

    sys in emergency mode, backing up

--- phase_03/feature_selection/fits_to_csv.py
+++ phase_03/feature_selection/fits_to_csv.py
@@ -28,9 +28,17 @@ def fits_to_csv(in_file , out_file , rows_file, class_int):
     print(data.shape)
     data.insert(int(0),'class',int(class_int)*np.ones(len(data)))
     data.to_csv(out_file)
+    rows_matrix = []
+    l = 0
+    for i in rows:
+        temp = [l,i]
+        rows_matrix.append(temp)
+        l+=1
+    rows_matrix = np.asarray(rows_matrix )
+    np.savetxt("rows/current_rows.csv", rows_matrix , fmt='%s')
     #data.insert(int(0),'class',np.zeros(len(data)))
     #data['class'] = int(1)
 print('CSV data')
-fits_to_csv('../data/CVs/CV_heasarc_x_chandra.fits', 'cv_data.csv', 'rows_non_str.json', 0)
+fits_to_csv('../data/CVs/CV_heasarc_x_chandra.fits', 'cv_data.csv', 'rows/rows_filter_level_1.json', 0)
 #print('LMXRB data')
-fits_to_csv('../data/binaries/LMXB_all.fits', 'lmxrb_all_data.csv', 'rows_non_str.json', 1)
+fits_to_csv('../data/binaries/LMXB_all.fits', 'lmxrb_all_data.csv', 'rows/rows_filter_level_1.json', 1)
